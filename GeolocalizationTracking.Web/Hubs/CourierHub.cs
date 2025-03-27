74713dd33950131a97f9ff7056a43e4960285ecf
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-27 18:43:36 -0500

    Create CourierHub class

---
+++ GeolocalizationTracking.Web/Hubs/CourierHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace GeolocalizationTracking.Web.Hubs
+{
+    public class CourierHub : Hub
+    {
+        private readonly ICourierTrackingService _trackingService;
+
+        public CourierHub(ICourierTrackingService trackingService)
+        {
+            _trackingService = trackingService;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            // Enviar todos los mensajeros activos al nuevo cliente
+            var activeCouriers = _trackingService.GetActiveCouriers();
+            await Clients.Caller.SendAsync("ReceiveActiveCouriers", activeCouriers);
+
+            await base.OnConnectedAsync();
+        }
+
+        public async Task RequestCourierLocations()
+        {
+            var activeCouriers = _trackingService.GetActiveCouriers();
+            await Clients.Caller.SendAsync("ReceiveActiveCouriers", activeCouriers);
+        }
+    }
+
+}
