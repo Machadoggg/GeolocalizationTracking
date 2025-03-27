632e85c01d31b3c28c8a82ce55018fe1e152d85b
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-27 18:48:08 -0500

    Add ICourierTrackingService interface

---
+++ GeolocalizationTracking.Web/Services/ICourierTrackingService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+
+namespace GeolocalizationTracking.Web.Services
+{
+    public interface ICourierTrackingService
+    {
+        void UpdateCourierLocation(CourierLocation location);
+        IEnumerable<CourierLocation> GetActiveCouriers();
+        CourierLocation GetCourierLocation(string courierId);
+    }
+
+    public class CourierTrackingService : ICourierTrackingService
+    {
+        private readonly ConcurrentDictionary<string, CourierLocation> _courierLocations = new();
+
+        public void UpdateCourierLocation(CourierLocation location)
+        {
+            _courierLocations.AddOrUpdate(location.CourierId, location, (id, old) => location);
+        }
+
+        public IEnumerable<CourierLocation> GetActiveCouriers()
+        {
+            return _courierLocations.Values;
+        }
+
+        public CourierLocation GetCourierLocation(string courierId)
+        {
+            _courierLocations.TryGetValue(courierId, out var location);
+            return location;
+        }
+    }
+}
