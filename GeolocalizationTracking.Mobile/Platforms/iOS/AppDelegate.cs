8e5007d89815ca52219422a4a1e4593e74d2ec43
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-27 22:53:43 -0500

    Create Mobile app project

---
+++ GeolocalizationTracking.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GeolocalizationTracking.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
