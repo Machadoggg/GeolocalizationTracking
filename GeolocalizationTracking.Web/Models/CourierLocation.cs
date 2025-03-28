4bd7d525cb6de51767fda001dfd712c789fd053a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-27 22:40:42 -0500

    Render Map

---
+++ GeolocalizationTracking.Web/Models/CourierLocation.cs
@@ -0,0 +1,10 @@
+﻿namespace GeolocalizationTracking.Web.Models
+{
+    public class CourierLocation
+    {
+        public string CourierId { get; set; } = default!;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
--- GeolocalizationTracking.Web/Services/ICourierTrackingService.cs
+++ GeolocalizationTracking.Web/Services/ICourierTrackingService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using GeolocalizationTracking.Web.Models;
+using System.Collections.Concurrent;
 
 namespace GeolocalizationTracking.Web.Services
 {
