acb3645575844a1c61d5420af44a8ab3bb758ebc
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-27 22:57:41 -0500

    Add ILocationService

---
+++ GeolocalizationTracking.Mobile/ILocationService.cs
@@ -0,0 +1,9 @@
+﻿namespace GeolocalizationTracking.Mobile
+{
+    public interface ILocationService
+    {
+        Task<Location> GetCurrentLocation();
+        Task StartTracking(string courierId);
+        Task StopTracking();
+    }
+}
